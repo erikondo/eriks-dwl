2f696fe63d45f4b8a73d7a00b41674e40289e5f0
Erik VanBaalen <erikvanbaalen@gmail.com>
2025-01-27 12:51:36 -0600

    Changed wallpaper script

--- config.def.h
+++ config.def.h
@@ -34,7 +34,7 @@ static int log_level = WLR_ERROR;
 
 /* Autostart */
 static const char *const autostart[] = {
-        "/home/erik/.local/share/dwl/autostart.sh", NULL,
+        "/home/erik/.local/share/dwl/wallpapers.sh", NULL,
         "fcitx5", "-d", NULL,
         "dunst", NULL,
         "kdeconnect-cli", NULL,
